d2bb6e56ed4c7961e9e5043ef17439b141a10d6e
Qplaysuper <102419809+QPLAYid@users.noreply.github.com>
2023-05-22 19:26:51 +0900

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.ohzegame.ramboshooter.brothersquad"
+#define GamePackageName "com.gear2.growslayer"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
