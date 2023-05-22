16907faa6f773eab81e05f7a791a55bf86cff474
Qplaysuper <102419809+QPLAYid@users.noreply.github.com>
2023-05-22 19:09:57 +0900

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.ohzegame.ramboshooter.brothersquad"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
