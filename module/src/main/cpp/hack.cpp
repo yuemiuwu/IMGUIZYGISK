cfaf6106576146abfe28e9e470b3c7d53d23abc9
Qplaysuper <102419809+QPLAYid@users.noreply.github.com>
2023-05-23 20:55:16 +0900

    Update hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -190,8 +190,8 @@ EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
     
     
     
-      if (ImGui::Begin("TuanMeta", nullptr)) {
-      if (ImGui::BeginTabBar("Mod by AMIYA | telegram: @MyAlessa", ImGuiTabBarFlags_None)) 
+      
+      if (ImGui::BeginTabBar("Mod by AMIYA | telegram: @MyAlessa", nullptr)) 
 		       
 		       
 		       {
@@ -230,9 +230,9 @@ EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
         ImGui::EndTabBar(); 
       }
     
-    ImGui::End();
     
-    }
+    
+    
 
     ImGui::EndFrame();
     ImGui::Render();
