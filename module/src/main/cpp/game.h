7b5c3bd8f0de0189e8a6d4eaaa39c7b5cbd42fe7
Qplaysuper <102419809+QPLAYid@users.noreply.github.com>
2023-06-17 13:11:05 +0900

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.gear2.growslayer"
+#define GamePackageName "com.rsg.myheroesen"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
