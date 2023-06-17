0adc69da28ef613be95f71a72a68e03580545968
Qplaysuper <102419809+QPLAYid@users.noreply.github.com>
2023-06-17 13:42:19 +0900

    Add files via upload

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -49,12 +49,7 @@ HOOKAF(void, Input, void *thiz, void *ex_ab, void *ex_ac) {
 }
 
 
-bool Tuanmeta = false;
-bool Tuanmeta1 = false;
-bool hackerr = true;
-bool DataCheck_Hacker = true;
-bool HackerCo = true;
-bool StageInfo = false;
+bool nodamage = false;
 
 
 
@@ -62,112 +57,55 @@ bool StageInfo = false;
 
 
 
+void (*old_addProcessDamage)(void *instance, void *damageSettle);
+void addProcessDamage(void *instance, void *damageSettle)
+{
 
+     if(instance != NULL&&nodamage)
+     {
+     
+     return ({});
+     
+     
+     
+     }
+     
+     
 
-
-
-
-
-
-
-
-
-
-long (*old_addgold)(void *instance);
-long addgold(void *instance) {
-    if (instance != NULL && Tuanmeta) {
-       return 9000000000;
-    }
-    return old_addgold(instance);
+return old_addProcessDamage(instance, damageSettle);
 }
 
 
 
-float (*old_addexp)(void *instance);
-float addexp(void *instance) {
-    if (instance != NULL && Tuanmeta1) {
-       return 900000000;
-    }
-    return old_addexp(instance);
-}
+float slidervalue;
+void (*old_playerProcessDamage)(void *instance, void *damageSettle);
+void playerProcessDamage(void *instance, void *damageSettle)
+{
+
+  if(instance != NULL)
+  
+  {
+  
+  if(damageSettle && slidervalue > 0)
+  
+  
+  {
+  *(bool*)((uintptr_t)damageSettle + 0x2c) = true;
+    *(float*)((uintptr_t)damageSettle + 0x34) = 90000 * slidervalue;
+  
+  
+  }
+  
+  
+  
+  }
+  
+  
 
 
-
-
-
-//anti cheat here
-
-bool (*old_addcheat)(void *instance, bool firstCheat);
-bool addcheat(void *instance, bool firstCheat) {
-    if (instance != NULL && hackerr) {
-       return false;
-    }
-    return old_addcheat(instance, firstCheat);
-}
-
-
-
-int (*old_DataCheck_H)(void *instance);
-int DataCheck_H(void *instance) {
-    if (instance != NULL && DataCheck_Hacker) {
-       return 0;
-    }
-    return old_DataCheck_H(instance);
-}
-
-
-
-void (*old_HackerCount)(void *instance);
-void HackerCount(void *instance) {
-    if (instance != NULL && HackerCo){
-		
-	
-    
-    *(int *) ((uint64_t) instance + 0x2110) = 0;
-      *(int *) ((uint64_t) instance + 0x2114) = 0;
-       *(bool *) ((uint64_t) instance + 0x2118) = false;
-      *(int *) ((uint64_t) instance + 0x308) = 0;
-      
-      *(int *) ((uint64_t) instance + 0x30c) = 0;
-	
-	}
-    
-    old_HackerCount(instance);
-}
-
-
-
-
-
-void (*old_addStageInfo)(void *instance);
-
-void addStageInfo(void *instance) {
-
-    if (instance != NULL && StageInfo){
-
-			
-
-    
-
-    *(float *) ((uint64_t) instance + 0x178) = 100;
-
-      *(float *) ((uint64_t) instance + 0x138) = 100;
-
-      
-
-	
-
-	}
-
-    
-
-    old_addStageInfo(instance);
+return old_playerProcessDamage(instance, damageSettle);
 
 }
-
-
-
-
 
 
 
@@ -258,9 +196,8 @@ EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
 	    
 	    
 
-    ImGui::Checkbox("Gold Hack", &Tuanmeta);
-    ImGui::Checkbox("Xp Hack", &Tuanmeta1);
-	ImGui::Checkbox("Drop Item Rare", &StageInfo);
+    ImGui::Checkbox("Gold Hack", &nodamage);
+    
     
 	    
 
@@ -271,12 +208,23 @@ EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
 			       
 			       
 
-    if (ImGui::BeginTabItem("Hack Multiplier")) 
+    if (ImGui::BeginTabItem("Hack Multiplier")){
     
     
-    {
-
+    ImGui::SliderFloat("Damage", &slidervalue, 0, 1000);
        
+       
+       
+       
+       
+       
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 	    
 	    
 
@@ -311,9 +259,18 @@ void hack_start(const char *_game_data_dir) {
 
 
 
-      //DobbyHook(getAbsAddress(0x3f57d04), (void*) addgold, (void**)&old_addgold);
+DobbyHook(getAbsAddress(0x33710b8), (void*) addProcessDamage, (void**)&old_addProcessDamage);
 
 
+DobbyHook(getAbsAddress(0x337495c), (void*) playerProcessDamage, (void**)&old_playerProcessDamage);
+
+
+
+
+
+
+
+      //
 
 
 
